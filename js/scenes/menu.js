9ac5682fd4f4f3f8ad3304294b329c7961cf90ce
jennahenricsson-umain <jenna.henricsson@umain.com>
2026-03-12 14:44:59 +0100

    Update menu.js

--- js/scenes/menu.js
+++ js/scenes/menu.js
@@ -1,4 +1,5 @@
 export function renderMenu(overlay, gesture, confidence) {
+
     overlay.innerHTML = `
         <img src="public/assets/Umain-logotype-white.png" class="main-menu-logo" alt="Logo">
         <p class="scene-text scene-text--main-menu">Main Menu<br>(Thumbs Up to start)</p>
