c5f7173f8db2ae7394d16feab82490271beeb7e1
jennahenricsson-umain <jenna.henricsson@umain.com>
2026-04-13 15:56:43 +0200

    single/multiplayer bilder för menu

--- js/scenes/menu.js
+++ js/scenes/menu.js
@@ -3,9 +3,11 @@ export function renderMenu(overlay, gesture, gesture2, confidence, confidence2, 
         <p class="scene-text scene-text--main-menu">
             CHOOSE <span class="highlight-orange">MODE</span>
         </p>
+        <img class="scene-image scene-image--left" src="public/assets/open_palm_chrome_left_JIN.png" alt="Menu Image">
         <p class="scene-text scene-text--menu-subtitle" style="left: 25%;">
             1 PLAYER<br>WAVE ON THE <span class="highlight-violet">LEFT</span>
         </p>
+        <img class="scene-image scene-image--right" src="public/assets/double_hands_JIN.png" alt="Menu Image">
         <p class="scene-text scene-text--menu-subtitle" style="left: 75%;">
             2 PLAYERS<br>WAVE ON THE <span class="highlight-orange">RIGHT</span>
         </p>
