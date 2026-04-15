969f6125a2f6584b6b179b8375c0a8e73b251914
jennahenricsson-umain <jenna.henricsson@umain.com>
2026-04-15 12:46:13 +0200

    lite cleanup i onboarding/start
    gjorde också the countdown i singleplayer orange

--- js/main.js
+++ js/main.js
@@ -102,6 +102,8 @@ function enterOnboarding() {
 function enterCountdown() {
     overlay.innerHTML = '';
     particles.innerHTML = '';
+    particlesP1.innerHTML = '';
+    particlesP2.innerHTML = ''; 
     overlay.dataset.countdownStep = '';
     countdownStart = Date.now();
     gameState = 'countdown';
