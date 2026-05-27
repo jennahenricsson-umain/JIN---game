f4c58816f22af1ed57d06bc3a9aad705e15e2464
jennahenricsson-umain <jenna.henricsson@umain.com>
2026-05-27 11:05:26 +0200

    Sessions groupas med datum
    Så nu visas bara dagens scores i scoreboarden. Dessutom måste man ha scannat qr och skrivit in för att synas i scoreboard.

--- js/firebase.js
+++ js/firebase.js
@@ -27,16 +27,18 @@ const db = getDatabase(initializeApp(firebaseConfig));
 
 export let currentSessionId = null;
 export let currentSessionId2 = null;
+export let gameStartDate = null;
 let gameStartTime = null;
 
 export function startGame(isMultiplayer = false) {
-    const newRef = push(ref(db, "sessions"));
-    currentSessionId = newRef.key;
     gameStartTime = Date.now();
+    gameStartDate = new Date().toISOString().slice(0,10);
+    const newRef = push(ref(db, `sessions/${gameStartDate}`));
+    currentSessionId = newRef.key;
     set(newRef, { timestamp: Date.now() });
 
     if (isMultiplayer) {
-        const newRef2 = push(ref(db, "sessions"));
+        const newRef2 = push(ref(db, `sessions/${gameStartDate}`));
         currentSessionId2 = newRef2.key;
         set(newRef2, { timestamp: Date.now() });
     } else {
@@ -47,9 +49,9 @@ export function startGame(isMultiplayer = false) {
 export function endGame(score, score2 = null) {
     if (!currentSessionId) return;
     const duration = Math.floor((Date.now() - gameStartTime) / 1000);
-    update(ref(db, `sessions/${currentSessionId}`), { score, duration });
+    update(ref(db, `sessions/${gameStartDate}/${currentSessionId}`), { score, duration });
     if (currentSessionId2 && score2 !== null) {
-        update(ref(db, `sessions/${currentSessionId2}`), {
+        update(ref(db, `sessions/${gameStartDate}/${currentSessionId2}`), {
             score: score2,
             duration,
         });
@@ -58,7 +60,7 @@ export function endGame(score, score2 = null) {
 
 export function watchUsername(sessionId, callback) {
     const unsubscribe = onValue(
-        ref(db, `sessions/${sessionId}/username`),
+        ref(db, `sessions/${gameStartDate}/${sessionId}/username`),
         (snapshot) => {
             const name = snapshot.val();
             if (name && name !== "Guest") {
@@ -71,7 +73,7 @@ export function watchUsername(sessionId, callback) {
 
 export async function fetchLeaderboard(limit = 5) {
     const q = query(
-        ref(db, "sessions"),
+        ref(db, `sessions/${gameStartDate}`),
         orderByChild("score"),
         limitToLast(limit)
     );
@@ -79,7 +81,7 @@ export async function fetchLeaderboard(limit = 5) {
     const results = [];
     snapshot.forEach((child) => {
         const val = child.val();
-        if (val.score != null)
+        if (val.score != null && val.username != null)
             results.push({ score: val.score, name: val.username || "" });
     });
     return results.sort((a, b) => b.score - a.score);
